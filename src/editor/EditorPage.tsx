38d7ab56421cc28a5b2b4b967b90dca172974112
inho2736 <inho2736@gmail.com>
2021-04-15 23:39:16 +0900

    [Feat] : Editor 샘플 컴포넌트 작성

---
+++ src/editor/EditorPage.tsx
@@ -0,0 +1,66 @@
+import React, { useRef, useEffect } from 'react';
+import { Editor } from '@toast-ui/react-editor';
+import '@toast-ui/editor/dist/toastui-editor.css';
+import 'Style/style.css';
+
+const EditorPage = (): JSX.Element => {
+  const editorRef = useRef<Editor>(null);
+
+  const clickConsole = () => {
+    if (editorRef.current !== null) {
+      /* eslint-disable no-console */
+      console.log(editorRef.current.getInstance().getSquire().getBody());
+    }
+  };
+  const clickSave = () => {
+    if (editorRef.current !== null) {
+      localStorage.setItem(
+        'content',
+        editorRef.current.getInstance().getSquire().getBody().innerHTML,
+      );
+    }
+  };
+
+  const clickLoad = () => {
+    if (editorRef.current !== null) {
+      const content = localStorage.getItem('content');
+      if (content) {
+        editorRef.current.getInstance().setHtml(content);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (editorRef.current !== null) {
+      editorRef.current
+        .getInstance()
+        .setHtml(
+          `<table class="custom"><thead><tr><th><br></th><th><br></th></tr></thead><tbody><tr><td><br><br><br><br><br><br><br></td><td><br><br><br><br><br><br><br></td></tr><tr><td><br><br><br><br><br><br><br></td><td><br><br><br><br><br><br><br></td></tr></tbody></table>`,
+        );
+    }
+  }, []);
+  return (
+    <>
+      <Editor
+        initialValue="hello"
+        previewStyle="vertical"
+        height="600px"
+        initialEditType="wysiwyg"
+        useCommandShortcut
+        ref={editorRef}
+        hideModeSwitch
+      />
+      <button onClick={clickConsole} type="button">
+        console Html
+      </button>
+      <button onClick={clickSave} type="button">
+        save Html
+      </button>
+      <button onClick={clickLoad} type="button">
+        load Html
+      </button>
+    </>
+  );
+};
+
+export default EditorPage;
