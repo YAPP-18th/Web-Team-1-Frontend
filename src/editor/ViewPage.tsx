2863947b7946d8b3c834dbf4a69be061f52a82c3
inho2736 <inho2736@gmail.com>
2021-04-15 23:39:47 +0900

    [Feat] : View 샘플 컴포넌트 작성

---
+++ src/editor/ViewPage.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { Viewer } from '@toast-ui/react-editor';
+import styled from 'styled-components';
+import '@toast-ui/editor/dist/toastui-editor.css';
+import 'Style/style.css';
+
+const StyledViewPage = styled.div`
+  width: 1000px;
+  background-color: white;
+  padding: 20px;
+`;
+
+const ViewPage = (): JSX.Element => {
+  const viewerRef = useRef<Viewer>(null);
+  const [value] = useState('<p>hello~</p>');
+  useEffect(() => {
+    if (viewerRef.current !== null) {
+      const content = localStorage.getItem('content');
+      if (content) {
+        viewerRef.current.getInstance().setMarkdown(content);
+      }
+    }
+  }, []);
+
+  return (
+    <StyledViewPage>
+      <Viewer initialValue={value} ref={viewerRef} />
+    </StyledViewPage>
+  );
+};
+
+export default ViewPage;
