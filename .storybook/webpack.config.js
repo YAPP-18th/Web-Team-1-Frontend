621764b04b09b03b72cf066f9e4e951c8490a35b
CharmingCheol <toby0806@naver.com>
2021-05-01 19:23:49 +0900

    Build : Storybook 세팅

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = ({ config }) => {
+  const fileLoaderRule = config.module.rules.find((rule) => rule.test.test('.svg'));
+  fileLoaderRule.exclude = /\.svg$/;
+  config.module.rules.push({
+    test: /\.svg$/,
+    use: ['@svgr/webpack'],
+  });
+  return config;
+};
