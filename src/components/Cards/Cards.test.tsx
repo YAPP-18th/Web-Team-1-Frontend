eb5457d71b3e5b01a7983f285972935f7a2ad75e
Kwakcena <rhkrgudwh@gmail.com>
2021-05-03 23:01:27 +0900

    [Feat]: Cards 컴포넌트에 cards props 추가
    Cards 컴포넌트가 화면에 보여줄 cards 목록을 props로 받아 보여주도록
    한다.

--- src/components/Cards/Cards.test.tsx
+++ src/components/Cards/Cards.test.tsx
@@ -4,12 +4,15 @@ import { render } from '@testing-library/react';
 
 import Cards from './index';
 
+import { cardsFixtures } from '../../fixtures/cards';
+
 describe('Cards', () => {
   it('카드 형태의 게시글을 볼 수 있다.', () => {
-    const { container } = render(<Cards />);
+    const { container } = render(<Cards cards={cardsFixtures} />);
 
-    expect(container).toHaveTextContent(
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum illo optio esse expedita iusto, repellat commodi dolorum! Minima, veniam provident. Ex non voluptate tempora dolorem repellendus voluptates, ipsa nihil autem?',
-    );
+    cardsFixtures.forEach(({ title, description }) => {
+      expect(container).toHaveTextContent(title);
+      expect(container).toHaveTextContent(description);
+    });
   });
 });
