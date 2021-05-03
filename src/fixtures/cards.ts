df37c464b65fb63b80a8645ccdb3537623140571
Kwakcena <rhkrgudwh@gmail.com>
2021-05-03 23:00:18 +0900

    [Feat]: fixtures 데이터 추가
    테스트 할 수 있는 card에 대한 fixtures 데이터를 추가한다.

---
+++ src/fixtures/cards.ts
@@ -0,0 +1,50 @@
+export const cardsFixtures = [
+  {
+    id: 1,
+    title: 'test title1',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum illo optio esse expedita iusto, repellat commodi dolorum! Minima, veniam provident. Ex non voluptate tempora dolorem repellendus voluptates, ipsa nihil autem?',
+  },
+  {
+    id: 2,
+    title: 'test title2',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum illo optio esse expedita iusto, repellat commodi dolorum! Minima, veniam provident. Ex non voluptate tempora dolorem repellendus voluptates, ipsa nihil autem?',
+  },
+  {
+    id: 3,
+    title: 'test title3',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum illo optio esse expedita iusto, repellat commodi dolorum! Minima, veniam provident. Ex non voluptate tempora dolorem repellendus voluptates, ipsa nihil autem?',
+  },
+  {
+    id: 4,
+    title: 'test title4',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum illo optio esse expedita iusto, repellat commodi dolorum! Minima, veniam provident. Ex non voluptate tempora dolorem repellendus voluptates, ipsa nihil autem?',
+  },
+  {
+    id: 5,
+    title: 'test title5',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum illo optio esse expedita iusto, repellat commodi dolorum! Minima, veniam provident. Ex non voluptate tempora dolorem repellendus voluptates, ipsa nihil autem?',
+  },
+  {
+    id: 6,
+    title: 'test title6',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum illo optio esse expedita iusto, repellat commodi dolorum! Minima, veniam provident. Ex non voluptate tempora dolorem repellendus voluptates, ipsa nihil autem?',
+  },
+  {
+    id: 7,
+    title: 'test title7',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum illo optio esse expedita iusto, repellat commodi dolorum! Minima, veniam provident. Ex non voluptate tempora dolorem repellendus voluptates, ipsa nihil autem?',
+  },
+  {
+    id: 8,
+    title: 'test title8',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum illo optio esse expedita iusto, repellat commodi dolorum! Minima, veniam provident. Ex non voluptate tempora dolorem repellendus voluptates, ipsa nihil autem?',
+  },
+];
