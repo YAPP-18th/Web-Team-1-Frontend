35b65a43cc7e06d8830b00f0451f81134bea1732
CharmingCheol <toby0806@naver.com>
2021-05-08 11:31:13 +0900

    [Refactor] : Global Style 스타일 추가 모든 태그에 font-size, border-box 적용. a 태그에 fit-content 적용

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,7 +1,13 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    font-size:1rem;
+  }
+  
   a {
+    width: fit-content;
     text-decoration: none;
     color: black;
   }
