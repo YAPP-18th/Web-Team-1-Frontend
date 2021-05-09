6593dfeeaa39fa4b7743a55ab66722a456fc07a2
inho2736 <inho2736@gmail.com>
2021-05-09 13:55:23 +0900

    [Fix] : 함수 위치수정, cancelAnimationFrame 추가

--- src/components/ScrollBar/ScrollBarContent.tsx
+++ src/components/ScrollBar/ScrollBarContent.tsx
@@ -3,30 +3,33 @@ import * as S from './style';
 
 const ScrollBarContent = () => {
   const [percent, setPercent] = useState(0);
-  const updateScrollPercent = () => {
-    const scrollPercent =
-      (window.scrollY / (document.body.offsetHeight - window.innerHeight)) * 100;
-    setPercent(scrollPercent);
-  };
-
-  const scrollPage = (update: () => void) => {
-    let tick = false;
-
-    return function onScroll(): number {
-      if (tick) {
-        return 0;
-      }
-      tick = true;
-      return requestAnimationFrame(function task() {
-        tick = false;
-        return update();
-      });
-    };
-  };
 
   useEffect(() => {
+    let raf: number;
+    const updateScrollPercent = () => {
+      const scrollPercent =
+        (window.scrollY / (document.body.offsetHeight - window.innerHeight)) * 100;
+      setPercent(scrollPercent);
+    };
+    const scrollPage = (update: () => void) => {
+      let tick = false;
+      return function onScroll(): number {
+        if (tick) {
+          return 0;
+        }
+        tick = true;
+        raf = requestAnimationFrame(function task() {
+          tick = false;
+          return update();
+        });
+        return raf;
+      };
+    };
     window.addEventListener('scroll', scrollPage(updateScrollPercent));
-    return () => window.removeEventListener('scroll', scrollPage(updateScrollPercent));
+    return () => {
+      cancelAnimationFrame(raf);
+      window.removeEventListener('scroll', scrollPage(updateScrollPercent));
+    };
   }, []);
 
   return <S.ScrollBarContent percent={percent} />;
