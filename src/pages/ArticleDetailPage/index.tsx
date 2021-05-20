a96ef3ad6e661489900a3b1dcc3386e3b80d9cf1
inho2736 <inho2736@gmail.com>
2021-05-20 09:02:11 +0900

    [Feat] : Article Detail Page 추가

---
+++ src/pages/ArticleDetailPage/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ArticleDetailCard, ProgressBar } from '#components/ArticleDetail';
+
+const ArticleDetailPage = () => {
+  return (
+    <>
+      <ProgressBar />
+      <ArticleDetailCard />
+    </>
+  );
+};
+
+export default ArticleDetailPage;
