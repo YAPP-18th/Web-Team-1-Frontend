be8b75263a2e759d5fac6bdc47e2d6c1e661a67e
Kwakcena <rhkrgudwh@gmail.com>
2021-05-25 01:13:18 +0900

    [Feat]: 스토리북에 카테고리 목록 추가
    storybook에서 카테고리 checkbox 그룹을 확인할 수 있도록 추가합니다.

--- src/components/Atoms/index.ts
+++ src/components/Atoms/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './Button';
+export { default as Categories } from './Categories';
 export { default as IconPaths } from './Icon/IconPaths';
 export { default as IconWrapper } from './Icon/IconWrapper';
