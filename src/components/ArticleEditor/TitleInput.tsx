81e714b374773d235b3a4dd395a533fe6073d6b6
inho2736 <inho2736@gmail.com>
2021-05-26 15:29:56 +0900

    [Feat] : Article Editor 페이지에서 사용하는 컴포넌트들 추가 TitleInput 컴포넌트로 Article 제목 적는 컴포넌트를 생성했습니다.

---
+++ src/components/ArticleEditor/TitleInput.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface Props {
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+const StyledTitleInput = styled.div`
+  & .title-input {
+    border: none;
+    font-family: Apple SD Gothic Neo;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 56px;
+    line-height: 56px;
+    letter-spacing: -0.04em;
+    margin-bottom: 64px;
+    padding-left: 30px;
+    padding-top: 30px;
+  }
+
+  & .title-input:focus {
+    outline: none;
+  }
+  & .title-input::placeholder {
+    color: #cdccc6;
+  }
+`;
+
+const TitleInput = ({ onChange }: Props) => {
+  return (
+    <StyledTitleInput>
+      <input className="title-input" placeholder="제목을 입력하세요" onChange={onChange} />
+    </StyledTitleInput>
+  );
+};
+
+export default TitleInput;
