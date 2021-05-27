3edec280d644a0c8e242ae173a8444363deba996
inho2736 <inho2736@gmail.com>
2021-05-27 22:57:35 +0900

    [Feat] : store에 articleEditorReducer 병합

--- src/slices/index.ts
+++ src/slices/index.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import cardsReducer from './cardsSlice';
+import articleEditorReducer from './articleEditorSlice';
 
 export const store = configureStore({
   reducer: {
     cardsReducer,
+    articleEditorReducer,
   },
 });
 
