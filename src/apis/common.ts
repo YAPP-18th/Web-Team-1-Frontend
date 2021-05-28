1cefa95b5a27614e02ee9c565cd51d8bf95de5fc
inho2736 <inho2736@gmail.com>
2021-05-29 00:10:24 +0900

    [Feat] : 토큰변경

--- src/apis/common.ts
+++ src/apis/common.ts
@@ -4,7 +4,7 @@ const instance = axios.create({
   baseURL: 'http://15.165.67.119:9000/api/v1/',
   headers: {
     // 글 작성은 로그인이 필요한 기능이므로, 임시로 토큰을 받아와 헤더에 더했습니다.
-    Authorization: `Bearer eyJhbGciOiJIUzUxMiJ9.eyJhdXRoIjoiUk9MRV9NRU1CRVIiLCJuaWNrbmFtZSI6IuyehOyduO2YuCIsInVzZXJfaWR4IjozLCJleHAiOjE2MjIxODAyMzMsImlhdCI6MTYyMjE3OTkzM30.HZ_e_rX917UErEyH5fcocbARDWzK4-1FPiMyd19c4-MGS-f3kx_5juXKCIRnc5vgfsxFVOHZVgW8cvPSAdpIvw`,
+    Authorization: `Bearer eyJhbGciOiJIUzUxMiJ9.eyJhdXRoIjoiUk9MRV9NRU1CRVIiLCJuaWNrbmFtZSI6IuyehOyduO2YuCIsInVzZXJfaWR4IjozLCJleHAiOjE2MjIyMTQ4OTMsImlhdCI6MTYyMjIxNDU5M30.9O2mkNOUyxppnte1A81LPwsHwDlVDm_MxbKNPUaPNuurzM1r3h-KE_askQ3JUntWdnn-kKJ33yrx95Y36N9Zfw`,
   },
 });
 
