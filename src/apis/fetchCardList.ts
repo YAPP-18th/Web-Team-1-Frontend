59be15fa94a61979542898475636038a67d1232f
Kwakcena <rhkrgudwh@gmail.com>
2021-05-29 15:49:47 +0900

    [Feat]: 카드 리스트를 불러오는 api 작성
    메인 페이지의 글 목록인 카드 리스트를 불러오는 api를 작성하였습니다.

---
+++ src/apis/fetchCardList.ts
@@ -0,0 +1,26 @@
+import instance from '#apis/common';
+
+interface Card {
+  postIdx: number;
+  title: string;
+  category: string;
+  contents: string;
+  nickname: string;
+  profile: string;
+  tag: string;
+  view: number;
+  created_at: string;
+  commentCnt: number;
+  scrapCnt: number;
+}
+
+export const fetchCardList = async (): Promise<Card[]> => {
+  const url = `http://ec2-15-165-67-119.ap-northeast-2.compute.amazonaws.com/api/v1/posts/lists?page=1&pageSize=20`;
+
+  try {
+    const { data } = await instance.get(url);
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
