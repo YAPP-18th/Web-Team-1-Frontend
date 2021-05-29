909e641703655dc900f0617f3d807abb5a21d9fb
inho2736 <inho2736@gmail.com>
2021-05-29 18:57:59 +0900

    [Feat] : ConfirmModalContainer 작성
    ConfirmModal의 컨테이너 컴포넌트 작성

---
+++ src/containers/ConfirmModalContainer/index.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import ConfirmModal from '#components/ConfirmModal';
+
+interface Props {
+  type: string;
+  onClick: () => void;
+}
+
+interface MsgSet {
+  header: string;
+  body: string;
+  btnMsg: string;
+}
+
+const Msg: { [key: string]: MsgSet } = {
+  write: {
+    header: '회고 작성을 완료할까요?',
+    body: '회고 작성을 완료한 이후에는 회고 카테고리, 해시태그 변경이 불가능합니다.',
+    btnMsg: '작성 완료',
+  },
+  delete: {
+    header: '회고글을 삭제할까요?',
+    body: '회고글은 한 번 삭제하면 되돌릴 수 없으므로 신중하게 확인해주세요.',
+    btnMsg: '삭제',
+  },
+};
+
+const ConfirmModalContainer = ({ type, onClick }: Props) => {
+  const [modal, setModal] = useState(false);
+  const toggle = () => setModal(!modal);
+
+  return (
+    <>
+      {modal && (
+        <ConfirmModal
+          toggle={toggle}
+          header={Msg[type].header}
+          body={Msg[type].body}
+          btnMsg={Msg[type].btnMsg}
+          onClick={onClick}
+        />
+      )}
+    </>
+  );
+};
+
+export default ConfirmModalContainer;
