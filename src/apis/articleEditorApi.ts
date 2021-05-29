b21994997d863cb426c382644f2aa75443633871
inho2736 <inho2736@gmail.com>
2021-05-29 16:21:51 +0900

    [Feat] : 해당 포스트의 템플릿을 받아와 에디터 설정

--- src/apis/articleEditorApi.ts
+++ src/apis/articleEditorApi.ts
@@ -19,3 +19,13 @@ export const postArticle = async (data: PostArticleData): Promise<number> => {
     return -1;
   }
 };
+
+export const getTemplate = async (templateIdx: number): Promise<string | null> => {
+  try {
+    const res = await instance.get(`/templates/${templateIdx}`);
+    return res.data.data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
