d475d2634dbea1ea931eefaf6791b624a7533d3b
Kwakcena <rhkrgudwh@gmail.com>
2021-05-30 01:46:42 +0900

    [Style]: dropdown을 클릭하면 list가 나오도록 한다
    - dropdown을 클릭하면 리스트 항목이 나온다.

--- src/components/Atoms/Dropdown/index.tsx
+++ src/components/Atoms/Dropdown/index.tsx
@@ -2,41 +2,70 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 export default function Dropdown() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggling = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <DropdownWrapper>
-      <DropdownHeader>최신순</DropdownHeader>
-      <DropdownList>
-        <DropdownItem>최신순</DropdownItem>
-        <DropdownItem>조회순</DropdownItem>
-      </DropdownList>
+      <DropdownHeader onClick={toggling}>최신순</DropdownHeader>
+      {isOpen && (
+        <DropdownList>
+          <DropdownItem>최신순</DropdownItem>
+          <DropdownItem>조회순</DropdownItem>
+        </DropdownList>
+      )}
     </DropdownWrapper>
   );
 }
 
 const DropdownWrapper = styled.div`
-  width: 98px;
-  height: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: #fefefe;
-
+  display: inline-block;
   font-family: Apple SD Gothic Neo;
   font-size: 18px;
   font-weight: 500;
   line-height: 18px;
   letter-spacing: -0.04em;
   color: #666666;
-
-  box-shadow: 0px 5px 20px rgba(205, 204, 198, 0.25);
-  border-radius: 54px;
 `;
 
-const DropdownHeader = styled.div``;
+const DropdownHeader = styled.div`
+  width: 98px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #fefefe;
+  box-shadow: 0px 5px 20px rgba(205, 204, 198, 0.25);
+  border-radius: 54px;
+  cursor: pointer;
+  position: relative;
+`;
 
 const DropdownList = styled.ul`
-  display: none;
+  position: absolute;
+  top: 61px;
+  left: 16px;
+  width: 99px;
+  height: 74px;
+  background: #fefefe;
+  border: 1px solid #eeeeee;
+  box-sizing: border-box;
+  box-shadow: 0px 5px 20px rgba(205, 204, 198, 0.25);
+  border-radius: 8px;
   list-style: none;
 `;
 
-const DropdownItem = styled.li``;
+const DropdownItem = styled.li`
+  width: 99px;
+  height: 38px;
+  padding-left: 10px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  &:hover {
+    background: yellow;
+  }
+`;
