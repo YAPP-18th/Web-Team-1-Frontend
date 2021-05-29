768212f1da5b10808bc0e0049446aeade74d9c56
Kwakcena <rhkrgudwh@gmail.com>
2021-05-30 02:29:52 +0900

    [Style]: style.ts 파일에 스타일 옮김
    스타일 관련 컴포넌트를 style 파일로 옮깁니다.

--- src/components/Atoms/Dropdown/index.tsx
+++ src/components/Atoms/Dropdown/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+import * as S from './style';
 import { IconWrapper, IconPaths } from '#components/Atoms';
 
 export default function Dropdown() {
@@ -24,77 +24,20 @@ export default function Dropdown() {
   const { isOpen, defaultOption, options } = state;
 
   return (
-    <DropdownWrapper>
-      <DropdownHeader onClick={toggling}>
+    <S.DropdownWrapper>
+      <S.DropdownHeader onClick={toggling}>
         {defaultOption}
         <IconWrapper icon={IconPaths.PolygonBottom} />
-      </DropdownHeader>
+      </S.DropdownHeader>
       {isOpen && (
-        <DropdownList>
+        <S.DropdownList>
           {options.map((option) => (
-            <DropdownItem key={option} onClick={() => handleClickOption(option)}>
+            <S.DropdownItem key={option} onClick={() => handleClickOption(option)}>
               {option}
-            </DropdownItem>
+            </S.DropdownItem>
           ))}
-        </DropdownList>
+        </S.DropdownList>
       )}
-    </DropdownWrapper>
+    </S.DropdownWrapper>
   );
 }
-
-const DropdownWrapper = styled.div`
-  display: inline-block;
-  font-family: Apple SD Gothic Neo;
-  font-size: 18px;
-  font-weight: 500;
-  line-height: 18px;
-  letter-spacing: -0.04em;
-  color: #666666;
-`;
-
-const DropdownHeader = styled.div`
-  width: 98px;
-  height: 40px;
-  padding-right: 20px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: #fefefe;
-  box-shadow: 0px 5px 20px rgba(205, 204, 198, 0.25);
-  border-radius: 54px;
-  cursor: pointer;
-  position: relative;
-  & span {
-    position: absolute;
-    right: 0px;
-    top: 8px;
-  }
-`;
-
-const DropdownList = styled.ul`
-  position: absolute;
-  top: 61px;
-  left: 16px;
-  width: 99px;
-  height: 78px;
-  background: #fefefe;
-  border: 1px solid #eeeeee;
-  box-sizing: border-box;
-  box-shadow: 0px 5px 20px rgba(205, 204, 198, 0.25);
-  border-radius: 8px;
-  list-style: none;
-`;
-
-const DropdownItem = styled.li`
-  width: 97px;
-  height: 38px;
-  padding-left: 10px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  &:hover {
-    color: #333333;
-    background: #f8f8f8;
-    border-radius: 8px;
-  }
-`;
---
+++ src/components/Atoms/Dropdown/style.ts
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+
+export const DropdownWrapper = styled.div`
+  display: inline-block;
+  font-family: Apple SD Gothic Neo;
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 18px;
+  letter-spacing: -0.04em;
+  color: #666666;
+`;
+
+export const DropdownHeader = styled.div`
+  width: 98px;
+  height: 40px;
+  padding-right: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #fefefe;
+  box-shadow: 0px 5px 20px rgba(205, 204, 198, 0.25);
+  border-radius: 54px;
+  cursor: pointer;
+  position: relative;
+  & span {
+    position: absolute;
+    right: 0px;
+    top: 8px;
+  }
+`;
+
+export const DropdownList = styled.ul`
+  position: absolute;
+  top: 61px;
+  left: 16px;
+  width: 99px;
+  height: 78px;
+  background: #fefefe;
+  border: 1px solid #eeeeee;
+  box-sizing: border-box;
+  box-shadow: 0px 5px 20px rgba(205, 204, 198, 0.25);
+  border-radius: 8px;
+  list-style: none;
+`;
+
+export const DropdownItem = styled.li`
+  width: 97px;
+  height: 38px;
+  padding-left: 10px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  &:hover {
+    color: #333333;
+    background: #f8f8f8;
+    border-radius: 8px;
+  }
+`;
