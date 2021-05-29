540350d21dcb1cea62fbb6f1ca9eae33f2eb7c1c
Kwakcena <rhkrgudwh@gmail.com>
2021-05-29 14:31:21 +0900

    [Style]: 배너 적용
    - 링크가 없는 배너를 적용합니다.

--- src/components/Atoms/Banner/index.tsx
+++ src/components/Atoms/Banner/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import * as S from './style';
 
 export default function Banner() {
   return (
-    <div>
-      <div>오늘 하루,</div>
-      <div>많은 생각을 했고,</div>
-      <div>회고가 필요하다면?</div>
-    </div>
+    <S.BannerWrapper>
+      <div className="first-line">
+        오늘 하루<span>, </span>
+      </div>
+      <div className="first-line">
+        많은 생각을 했고<span>, </span>
+      </div>
+      <div>
+        회고가 필요하다면<span>?</span>
+      </div>
+    </S.BannerWrapper>
   );
 }
