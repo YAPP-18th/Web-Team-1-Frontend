1cf202ff3d71fc5ffc279d272b74077f89ef8aa5
Kwakcena <rhkrgudwh@gmail.com>
2021-05-30 05:47:55 +0900

    [Feat]: 임시 버튼 삭제

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -4,7 +4,6 @@ import { Banner } from '#components/Atoms';
 
 import CardsContainer from '#containers/CardsContainer';
 import ConditionContainer from '#containers/ConditionContainer';
-import ArticleModalContainer from '#containers/ArticleModalContainer';
 
 export default function HomePage() {
   const history = useHistory();
@@ -20,9 +19,6 @@ export default function HomePage() {
         <Banner />
         <ConditionContainer />
         <CardsContainer onClickCard={handleClickCard} />
-
-        {/* 헤더의 바로 작성하기 버튼 대신 임시로 쓰이는 버튼입니다. */}
-        <ArticleModalContainer />
       </main>
     </>
   );
