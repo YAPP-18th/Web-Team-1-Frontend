e91d03167d3f811429906a0b81ce90bac351e73c
Kwakcena <rhkrgudwh@gmail.com>
2021-05-29 15:29:59 +0900

    [Build]: Thunk 타입 지정
    - ThunkAction에 대한 타입을 설정합니다.

--- src/slices/index.ts
+++ src/slices/index.ts
@@ -1,4 +1,5 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, Action } from '@reduxjs/toolkit';
+import { ThunkAction } from 'redux-thunk';
 
 import cardsReducer from './cardsSlice';
 import alertReducer from './alertSlice';
@@ -14,3 +15,4 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>;
