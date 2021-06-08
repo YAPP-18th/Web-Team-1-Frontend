32d98585224be03cb3e10cae70b2bc2a50e86b34
inho2736 <inho2736@gmail.com>
2021-06-08 22:49:27 +0900

    [Build] : 서버 url 포트번호 제거

--- src/apis/common.ts
+++ src/apis/common.ts
@@ -2,18 +2,18 @@ import axios from 'axios';
 import checkToken from '#apis/checkToken';
 
 const tokenInstance = axios.create({
-  baseURL: 'http://15.165.67.119:9000/api/v1/',
+  baseURL: 'http://15.165.67.119/api/v1/',
   headers: {
     Authorization: `Bearer ${window.localStorage.getItem('accessToken')}`,
   },
 });
 
 const instance = axios.create({
-  baseURL: 'http://15.165.67.119:9000/api/v1/',
+  baseURL: 'http://15.165.67.119/api/v1/',
 });
 
 const refreshInstance = axios.create({
-  baseURL: 'http://15.165.67.119:9000/api/v1/',
+  baseURL: 'http://15.165.67.119/api/v1/',
   headers: {
     Authorization: `Bearer ${window.localStorage.getItem('accessToken')}`,
   },
