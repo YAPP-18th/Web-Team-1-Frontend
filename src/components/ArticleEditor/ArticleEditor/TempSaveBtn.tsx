62c04719369d457ed013b6882349fb6e8dcf23d4
inho2736 <inho2736@gmail.com>
2021-06-10 23:29:44 +0900

    [Feat] : 임시 글설정(ArticleModal) 버튼 부착

--- src/components/ArticleEditor/ArticleEditor/TempSaveBtn.tsx
+++ src/components/ArticleEditor/ArticleEditor/TempSaveBtn.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Button } from '#components/Atoms/index';
-// import { color } from '#styles/index';
 
 interface Props {
   onClick: () => void;
