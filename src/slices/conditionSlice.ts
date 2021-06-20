22fa7b58f711452a9482b12742f47577e25ed889
Kwakcena <rhkrgudwh@gmail.com>
2021-06-21 00:05:46 +0900

    전체 버튼 클릭 시 초기 버튼 상태로 바뀜
    전체 버튼을 클릭하면 활성화 되어 있던 버튼들의 색이 회색으로 변하면서
    기본 상태로 돌아가게 하고, 다른 카테고리를 클릭하면 전체는 비활성,
    클릭한 버튼은 활성화 되도록 한다.

--- src/slices/conditionSlice.ts
+++ src/slices/conditionSlice.ts
@@ -4,23 +4,35 @@ import { fetchPostsWith } from '#apis/fetchCardList';
 import { setCards } from './cardsSlice';
 import { getQuery } from '../utils';
 
+const initialCheckedState = {
+  total: true,
+  marketing: false,
+  design: false,
+  plan: false,
+  develop: false,
+};
+
 const { actions, reducer: conditionReducer } = createSlice({
   name: 'condition',
   initialState: {
-    categories: {
-      total: false,
-      marketing: false,
-      design: false,
-      plan: false,
-      develop: false,
-    },
+    categories: initialCheckedState,
   },
   reducers: {
     setCategories(state, { payload: { id, checked } }) {
+      if (id === 'total') {
+        return {
+          ...state,
+          categories: {
+            ...initialCheckedState,
+          },
+        };
+      }
+
       return {
         ...state,
         categories: {
           ...state.categories,
+          total: false,
           [id]: checked,
         },
       };
