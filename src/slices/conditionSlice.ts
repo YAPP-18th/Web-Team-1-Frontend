6573c62ef0843ca7724348d83d5b494ef0694c76
Kwakcena <rhkrgudwh@gmail.com>
2021-06-20 16:10:11 +0900

    카테고리 버튼의 상태를 갖는 redux 초기 값을 정의
    각 버튼의 상태를 redux 내의 객체에서 boolean 값으로 정의한다.

--- src/slices/conditionSlice.ts
+++ src/slices/conditionSlice.ts
@@ -2,7 +2,15 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const { reducer: conditionReducer } = createSlice({
   name: 'condition',
-  initialState: {},
+  initialState: {
+    categories: {
+      total: false,
+      marketing: false,
+      design: false,
+      plan: false,
+      develop: false,
+    },
+  },
   reducers: {},
 });
 
