9dd4659777aca9c811c40a085a0fa12a613b7174
Kwakcena <rhkrgudwh@gmail.com>
2021-06-22 23:58:54 +0900

    isEmpty util 추가
    문자열, 배열, 객체가 비어있는지 판단해주는 isEmpty 함수를 추가한다.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,5 @@ export const getQuery = (object: { [key: string]: boolean }) =>
   Object.keys(object)
     .filter((key) => !!object[key])
     .toString();
+
+export const isEmpty = (val: any) => val == null || !(Object.keys(val) || val).length;
