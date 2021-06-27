4756ec214e4b1a49d3b28c23cb8246619dfb98f7
Kwakcena <rhkrgudwh@gmail.com>
2021-06-28 02:00:32 +0900

    전체가 활성화 되어 있으면 색깔을 활성화 한다
    카테고리 버튼 전체가 눌려져 있으면 색이 추가되고, 이 때 전체 버튼도
    활성화 되어야 한다.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,13 @@
-export const getQuery = (object: { [key: string]: boolean }) =>
-  Object.keys(object)
+export const getQuery = (object: { [key: string]: boolean }) => {
+  if (object.total) {
+    return 'total';
+  }
+  return Object.keys(object)
     .filter((key) => !!object[key])
     .toString();
+};
 
 export const isEmpty = (val: any) => val == null || !(Object.keys(val) || val).length;
 
 export const isAllDisable = (object: { [key: string]: boolean }) =>
-  Object.entries(object).some(([_, value]) => value);
+  !Object.entries(object).some(([_, value]) => value);
